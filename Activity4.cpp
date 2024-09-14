42a7ed3dff0dd969b4fd3c890cfaaffdbee93b55
Zak <80486082+Zak-Dev21@users.noreply.github.com>
2024-09-13 21:37:37 -0700

    Adjusting indents

--- Activity4.cpp
+++ Activity4.cpp
@@ -7,5 +7,5 @@ int main()
 {
     cout << left << setw(10) << "Course" << setw(10) << "Student" << endl;
     cout << right << setw(10) << "C++" << setw(10) << "50" << endl;
-	cout << right << setw(10) << "Java" << setw(10) << "100" << endl;
+    cout << right << setw(10) << "Java" << setw(10) << "100" << endl;
 }
