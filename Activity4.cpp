30b2e20fda21dc76b23f9ccea533a7fda1b62566
Zak <80486082+Zak-Dev21@users.noreply.github.com>
2024-09-13 21:39:16 -0700

    changes to number order

--- Activity4.cpp
+++ Activity4.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main()
 {
     cout << left << setw(10) << "Course" << setw(10) << "Student" << endl;
-    cout << right << setw(10) << "C++" << setw(10) << "50" << endl;
-    cout << right << setw(10) << "Java" << setw(10) << "100" << endl;
+    cout << right << setw(10) << "C++" << setw(10) << "100" << endl;
+    cout << right << setw(10) << "Java" << setw(10) << "50" << endl;
 }
