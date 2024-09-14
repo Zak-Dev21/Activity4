b9f8253161d858ecb7eb31a9f2d6415603bdd22c
Zak <zakariabouaddou@gmail.com>
2024-09-13 21:33:30 -0700

    Add project files.

---
+++ Activity4.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << left << setw(10) << "Course" << setw(10) << "Student" << endl;
+    cout << right << setw(10) << "C++" << setw(10) << "50" << endl;
+	cout << right << setw(10) << "Java" << setw(10) << "100" << endl;
+}
